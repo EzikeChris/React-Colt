34dc8d138135e45cac09317a802acd31ca38e2a3
ezikechris <christophermbe1009@gmail.com>
2020-12-09 14:09:18 +0100

    intial commit

---
+++ libraryvsframeworks.jsx
@@ -0,0 +1,11 @@
+// Librabries are  mostly codes which has been written by other developers which you would add to your websites or your projects//
+
+// examples of libraries in javascript include//
+
+// Boostraps
+// Jquery etc
+
+//Frameworks on the other hand 
+
+//Larger JS Libraries
+//Provide "blueprint" for apps a kind of structure or body to build your apps how you would prefer//
